dec3cafce8c0aff8411578a7a2287a29374adca5
Marcel-MD <marcelvlasenco@gmail.com>
2023-01-29 15:52:31 +0200

    Add mongoDB to docker compose, bootstrap file service

---
+++ file/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"sync"
+
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog/log"
+)
+
+type Config struct {
+	Port string `env:"PORT" envDefault:":8084"`
+}
+
+var (
+	once sync.Once
+	cfg  Config
+)
+
+func GetConfig() Config {
+	once.Do(func() {
+		err := godotenv.Load()
+		if err != nil {
+			log.Warn().Err(err).Msg("Failed to load .env file.")
+		}
+
+		if err := env.Parse(&cfg); err != nil {
+			log.Fatal().Err(err).Msg("Failed to parse environment variables.")
+		}
+	})
+
+	return cfg
+}
---
+++ file/go.mod
@@ -0,0 +1,16 @@
+module file
+
+go 1.19
+
+require (
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/joho/godotenv v1.4.0
+	github.com/rs/zerolog v1.29.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+)
